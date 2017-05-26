fcb9485079b746faf3f2793bc9821672036287c3
dpatelzc <dpatel2@zerochaos.com>
2017-05-26 18:51:20 +0530

    Added Culture related changes on IOS project

---
+++ ZCMobileDemo.Lite/ZCMobileDemo.Lite/ZCMobileDemo.Lite.iOS/Culture/CultureImplementation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using ZCMobileDemo.Lite.Interfaces;
+using System.Threading;
+
+[assembly: Dependency(typeof(ZCMobileDemo.Lite.iOS.Culture.CultureImplementation))]
+namespace ZCMobileDemo.Lite.iOS.Culture
+{
+    public class CultureImplementation : ILocalization
+    {
+        public CultureInfo GetCurrentCulture()
+        {
+            //var netLanguage = "en";
+            //var androidLocale = Java.Util.Locale.Default;
+            //var netLanguage = Thread.CurrentThread.CurrentCulture; //androidLocale.ToString().Replace("_", "-");
+            // this gets called a lot - try/catch can be expensive so consider caching or something
+            System.Globalization.CultureInfo ci = null;
+            try
+            {
+                ci = new System.Globalization.CultureInfo(Thread.CurrentThread.CurrentCulture.Name);
+            }
+            catch (CultureNotFoundException e1)
+            {
+
+            }
+            return ci;
+        }
+
+        public void SetCulture(CultureInfo cl)
+        {
+            Thread.CurrentThread.CurrentCulture = cl;
+        }
+    }
+}
